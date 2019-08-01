4ce633a2a297ce7deab46d6073dcc0824bcea482
Hackbright Student <no-reply@hackbrightacademy.com>
2019-08-01 12:55:16 -0700

    completed phone number and information and address displays

--- trials.js
+++ trials.js
@@ -2,28 +2,49 @@
 // PART 1
 // Account information:
 
-let accountHolder = "Balloonicorn";
-const accountNumber = "123456";
-let businessName = "Balloonicorn's Cupcake Shop";
+let accountHolder = 'Balloonicorn';
+const accountNumber = '123456';
+let businessName = `Balloonicorn's Cupcake Shop`;
 
-const addresses = ["123 Main Street", "683 Sutter Street", "1600 Pennsylvania Ave"];
+const addresses = ['123 Main Street', '683 Sutter Street', '1600 Pennsylvania Ave'];
 
 const phoneNumbers = new Map();
 
 // Add some phone numbers to our map
-
-
+phoneNumbers.set('home', '510-867-5309');
+phoneNumbers.set('mobile', '415-555-1212');
+phoneNumbers.set('business', '415-552-2121');
+console.log(phoneNumbers);
 
 // ///////////////////////////////////////////////////////
 // Create User Info Display:
 
-// Add function to print account information 
+// Add function to print account information
+function showInfo(name, number, busName) {
+  console.log(`Account Holder Name: ${name}`);
+  console.log(`Account Holder Number: ${number}`);
+  console.log(`Business Name: ${busName}`);
+}
 
+showInfo(accountHolder, accountNumber, businessName);
 
 // Add function to print all addresses, including a header
-
+function showAddresses(addressesList) {
+  console.log('Addresses:');
+  for (const address of addressesList) {
+    console.log(`${address}`);
+  }
+}
+showAddresses(addresses);
 
 // Add function to print phone types and numbers
+function showPhoneNums(phoneMap) {
+  console.log('Phone Numbers: ');
+  for (const [key, val] of phoneMap) {
+    console.log(key, ':', val);
+  }
+}
+showPhoneNums(phoneNumbers);
 
 
 // ///////////////////////////////////////////////////////
